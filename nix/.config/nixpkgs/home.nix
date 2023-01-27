3b1c0bae1b521ff27a3f9f86e4f648d00d72ffc3
feniljain <fkjainco@gmail.com>
2023-01-27 23:23:29 +0530

    feat: shift packages from brew to nix

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -8,7 +8,41 @@
 
   programs.home-manager.enable = true;
 
+ /*
+ Except of these packages, some are which still are preferred
+ to be installed manually like:
+    - java ( so JDK )
+    - neovim ( latest version not available on nixpkgs )
+ */
   home.packages = [
-    pkgs.cowsay
+    /* Must have */
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.stow
+    pkgs.tmux
+    pkgs.wget
+    pkgs.vifm
+    pkgs.htop
+
+    /* Good to have */
+    pkgs.bat
+    pkgs.cmake
+    pkgs.ffmpeg
+    pkgs.skaffold
+    pkgs.jq
+    pkgs.pgcli
+    pkgs.tldr
+    pkgs.fd
+    pkgs.difftastic
+
+    /*
+    Nice to have pkgs which are
+    already preinstalled in mac
+    */
+    /* pkgs.vim */
+    /* pkgs.curl */
+
+    /* Pkg not supported in mac */
+    /* pkgs.helm */
   ];
 }
