183d932bdf9f4b94e5983ea2e5a7812487e22e4c
feniljain <fkjainco@gmail.com>
2025-03-21 14:09:44 +0530

    fix: only load jdtls for java files

---
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -0,0 +1,27 @@
+local workspace_dir = "/Users/feniljain/.jdtls_data/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ':h:h:t') .. "/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ':h:t') .. "/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+
+local config = {
+    cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        "/Users/feniljain/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
+        "-configuration",
+        "/Users/feniljain/.local/share/nvim/mason/packages/jdtls/config_mac",
+        "-data",
+        workspace_dir,
+    },
+    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
+}
+
+require("jdtls").start_or_attach(config)
