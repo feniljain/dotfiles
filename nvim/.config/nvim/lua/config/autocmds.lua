604e419e9a447cea325c1f6fcbbc4f79f57d1685
feniljain <fkjainco@gmail.com>
2025-07-07 14:31:11 +0530

    upgrade: highlight API to hl

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -11,12 +11,12 @@ vim.api.nvim_create_autocmd("BufHidden", {
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank-u-la-la', { clear = true }),
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
 })
 
